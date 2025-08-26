67cb1427ec4355c87410ed140c6ac3ec5883177a
Raul Lima <raul.ferreira.2@hotmail.com>
2025-08-25 21:29:01 -0300

    [mfe]feature(base): adicionando projetos base e inclusão do module-federation

---
+++ workspace/projects/shell/webpack.config.js
@@ -0,0 +1,14 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "mfeCadastro": "http://localhost:4201/remoteEntry.js",
+    "mfeSucesso": "http://localhost:4202/remoteEntry.js",    
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
