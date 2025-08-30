0f22c3652e52dbacac1bbcea1b5ca564cd3305dc
Raul Lima <raul.ferreira.2@hotmail.com>
2025-08-30 15:25:19 -0300

    [mfe]feature(cadastro): finalizando componente de cadastro

--- workspace/projects/mfe-cadastro/src/app/components/formCadastro/form-cadastro.module.ts
+++ workspace/projects/mfe-cadastro/src/app/components/formCadastro/form-cadastro.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FormCadastroRoutingModule } from './form-cadastro-routing.module';
 import { RegisterUserComponent } from './register-user/register-user.component';
+import { RegisterUserService } from '../../services/register-user.service';
+
+import { FormCadastroRoutingModule } from './form-cadastro-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 @NgModule({
@@ -11,7 +21,15 @@ import { RegisterUserComponent } from './register-user/register-user.component';
   ],
   imports: [
     CommonModule,
-    FormCadastroRoutingModule
-  ]
+    FormCadastroRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule
+  ],
+  providers: [RegisterUserService]
 })
 export class FormCadastroModule { }
--- workspace/projects/mfe-cadastro/src/app/components/formCadastro/register-user/register-user.component.ts
+++ workspace/projects/mfe-cadastro/src/app/components/formCadastro/register-user/register-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterUserService } from '../../../services/register-user.service';
+import { Users } from '../../../models/user.interface';
+import { ApiFeedback } from '../../../models/messages.enum';
 
 @Component({
   selector: 'app-register-user',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-user.component.scss']
 })
 export class RegisterUserComponent {
+  public userForm!: FormGroup;
+  public successOrErrorMessage = '';
+  public feedBackMessage = ApiFeedback;
+  public showMessage: boolean = false;
 
+  constructor(
+    private registerUserService: RegisterUserService,
+    private fb: FormBuilder
+  ){
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  onSubmit(): void {
+    const newUser: Users = this.userForm.value;
+
+    this.registerUserService.postUser(newUser).subscribe((data) => {
+      this.successOrErrorMessage = data;
+      this.showMessage = true;
+      setInterval(() => {
+        this.showMessage = false;
+      },2500)
+      this.userForm.reset();
+    })
+  }
 }
